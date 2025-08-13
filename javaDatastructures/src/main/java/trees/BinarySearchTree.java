870afe1b55f9182a3d967452cdcc679aa9285279
Widua <mikolaj.widla@gmail.com>
2025-08-13 18:52:20 +0200

    Implemented deletion

--- javaDatastructures/src/main/java/trees/BinarySearchTree.java
+++ javaDatastructures/src/main/java/trees/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package trees;
 
+import java.util.NoSuchElementException;
+
 public class BinarySearchTree implements Tree {
 
     private int height;
@@ -28,12 +30,12 @@ public class BinarySearchTree implements Tree {
 
     @Override
     public void insert(Integer value) {
-        if (root == null){
+        if (root == null) {
             root = new BTNode(value);
             length++;
             return;
         }
-        recInsert(value,root);
+        recInsert(value, root);
         length++;
     }
 
@@ -43,33 +45,112 @@ public class BinarySearchTree implements Tree {
             curr.leftChild = new BTNode(value);
             return;
         }
-        if (curr.rightChild == null && curr.value < value){
+        if (curr.rightChild == null && curr.value < value) {
             curr.rightChild = new BTNode(value);
             return;
         }
-        if (curr.value >= value){
-            recInsert(value,curr.leftChild);
+        if (curr.value >= value) {
+            recInsert(value, curr.leftChild);
         }
-        if (curr.value < value){
-            recInsert(value,curr.rightChild);
+        if (curr.value < value) {
+            recInsert(value, curr.rightChild);
         }
     }
 
     @Override
     public void delete(Integer value) {
+        length--;
+
+        BTNode parent = recGetParent(value, root);
+        if (parent == null) {
+
+            throw new NoSuchElementException();
+        }
+        BTNode toDelete;
+
+        if (parent.value >= value) {
+            toDelete = parent.leftChild;
+            if (toDelete.leftChild == null && toDelete.rightChild == null) {
+                parent.leftChild = null;
+                return;
+            }
+            if (toDelete.leftChild == null) {
+                parent.leftChild = toDelete.rightChild;
+                return;
+            }
+            if (toDelete.rightChild == null) {
+                parent.leftChild = toDelete.leftChild;
+                return;
+            }
+
+            BTNode max = deleteWhenTwoChild(toDelete);
+            parent.leftChild = max;
+        }
+        if (parent.value < value) {
+            toDelete = parent.rightChild;
+            if (toDelete.leftChild == null && toDelete.rightChild == null) {
+                parent.rightChild = null;
+                return;
+            }
+            if (toDelete.leftChild == null) {
+                parent.rightChild = toDelete.rightChild;
+                return;
+            }
+            if (toDelete.rightChild == null) {
+                parent.rightChild = toDelete.leftChild;
+                return;
+            }
+
+            BTNode max = deleteWhenTwoChild(toDelete);
+            parent.rightChild = max;
+        }
 
     }
 
-    private boolean recFind(Integer value, BTNode curr){
-        if (curr.value.equals(value)){
+    private BTNode deleteWhenTwoChild(BTNode toDelete) {
+        BTNode maxParent = toDelete.leftChild;
+        BTNode max = maxParent;
+
+        while (maxParent.rightChild != null && maxParent.rightChild.rightChild != null) {
+            maxParent = maxParent.rightChild;
+        }
+
+        if (maxParent.rightChild != null) {
+            max = maxParent.rightChild;
+        }
+
+        maxParent.rightChild = null;
+        max.leftChild = toDelete.leftChild;
+        max.rightChild = toDelete.rightChild;
+
+        return max;
+    }
+
+    private BTNode recGetParent(Integer value, BTNode curr) {
+        if (curr.leftChild.value.equals(value) || curr.rightChild.value.equals(value)) {
+            return curr;
+        }
+
+        if (curr.value >= value && curr.leftChild != null) {
+            return recGetParent(value, curr.leftChild);
+        }
+        if (curr.value < value && curr.rightChild != null) {
+            return recGetParent(value, curr.rightChild);
+        }
+
+        return null;
+    }
+
+    private boolean recFind(Integer value, BTNode curr) {
+        if (curr.value.equals(value)) {
             return true;
         }
 
-        if (curr.value >= value && curr.leftChild != null){
-            return recFind(value,curr.leftChild);
+        if (curr.value >= value && curr.leftChild != null) {
+            return recFind(value, curr.leftChild);
         }
-        if (curr.value < value && curr.rightChild != null){
-            return recFind(value,curr.rightChild);
+        if (curr.value < value && curr.rightChild != null) {
+            return recFind(value, curr.rightChild);
         }
 
         return false;
@@ -77,7 +158,7 @@ public class BinarySearchTree implements Tree {
 
     @Override
     public boolean find(Integer value) {
-        return recFind(value,root);
+        return recFind(value, root);
     }
 
 
--- javaDatastructures/src/test/java/trees/BinarySearchTreeTest.java
+++ javaDatastructures/src/test/java/trees/BinarySearchTreeTest.java
@@ -43,5 +43,77 @@ class BinarySearchTreeTest {
         assertFalse(bst.find(1));
     }
 
+    @Test
+    public void noChildDeletionTest(){
+        BinarySearchTree bst = new BinarySearchTree();
 
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(2);
+        bst.insert(8);
+        bst.insert(15);
+        bst.insert(14);
+        bst.insert(13);
+
+        bst.delete(2);
+        assertFalse(bst.find(2));
+        assertEquals(6,bst.size());
+    }
+
+    @Test
+    public void oneChildDeletionTest(){
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(2);
+        bst.insert(8);
+        bst.insert(15);
+        bst.insert(14);
+        bst.insert(13);
+
+        bst.delete(14);
+        assertFalse(bst.find(14));
+        assertEquals(6,bst.size());
+        assertTrue(bst.find(13));
+        assertEquals(13, bst.getRoot().rightChild.leftChild.value);
+    }
+
+    @Test
+    public void twoChildNodeNoDeepDeletionTest(){
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(2);
+        bst.insert(8);
+        bst.insert(15);
+        bst.insert(14);
+        bst.insert(13);
+
+        bst.delete(5);
+
+        assertTrue(bst.find(8));
+        assertTrue(bst.find(2));
+        assertEquals(2,bst.getRoot().leftChild.value);
+    }
+
+    @Test
+    public void twoChildNodeDeepDeletionTest(){
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(2);
+        bst.insert(8);
+        bst.insert(15);
+        bst.insert(14);
+        bst.insert(13);
+        bst.insert(3);
+        bst.insert(4);
+
+        bst.delete(5);
+
+        assertEquals(4,bst.getRoot().leftChild.value);
+    }
 }
